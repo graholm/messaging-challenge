90840e2cae78aeceb14ee6f4c2a4e310fe474144
graholm <graholm@nuevaschool.org>
2016-09-30 09:01:06 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -5,18 +5,10 @@ var quantityInput = $(".element-quantity-input")
 console.log ("quantityInput");
 
 quantityInput.keydown(function(e) {
-  console.log ("ok")
-  var keyCode = e.13;
+  console.log (keyCode)
+  var keyCode = e;
   var inputValue = quantityInput.val();
-
   if (keyCode == 13) {	
     addElementsToThePage(inputValue);
   }
 })
-
-function addElementsToThePage(numberOfElements) {	  
-  for (var i = 0; i < 5; i++) {
-    var newEl = $("<div class='new-el'>Hi there!</div>");		
-    $(".element-display").append(newEl);
-  }
-}
